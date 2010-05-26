a0a72a84db2d8dcb05aa8fcbebf49230c09ebab8
Christoph Junghans <junghans@mpip-mainz.mpg.de>
2010-05-26 20:02:45 +0200

    updated logger clean_out

--- logs/clean_out.sh
+++ logs/clean_out.sh
@@ -1,10 +1,30 @@
 #! /bin/bash -x
 logfile="log.txt"
 cp $logfile ${logfile%.txt}_$(date +%y%m%d_%H%M).txt
-sed -i -e '/ (find.\.mpg.de) /d' \
-       -e '/ (crawl-[^)]*\.googlebot\.com) /d' \
-       -e '/ (msnbot-[^)]*\.search\.msn\.com) /d' \
-       -e '/ ([^)]*\.crawl\.yahoo\.net) /d' \
-       -e '/ (crawl.\.dotnetdotcom\.org) /d' \
-       -e '/ (crawler[^)]*\.ask\.com) /d' \
+sed -i \
+       -e '/mpg-crawler/d' \
+       -e '/Googlebot/d' \
+       -e '/msnbot/d' \
+       -e '/Slurp/d' \
+       -e '/discobot/d' \
+       -e '/SurveyBot/d' \
+       -e '/Twiceler/d' \
+       -e '/CCBot/d' \
+       -e '/Exabot/d' \
+       -e '/MJ12bot/d' \
+       -e '/citeseerxbot/d' \
+       -e '/MLBot/d' \
+       -e '/Yandex/d' \
+       -e '/LinkLint-checkonly/d' \
+       -e '/NutchCVS/d' \
+       -e '/Speedy Spider/d' \
+       -e '/minerva\/crawl/d' \
+       -e '/Ask Jeeves\/Teoma/d' \
+       -e '/crawler@alexa.com/d' \
+       -e '/FAST Enterprise Crawler/d' \
+       -e '/sSearch Crawler/d' \
+       -e '/conpilot crawler/d' \
+       -e '/Steeler/d' \
+       -e '/Yeti\//d' \
+       -e '/SSearch Crawler/d' \
        $logfile
