46aa85cfbde9f83bdf73dfdf5d4703f1681d740c
Christoph Junghans <junghans@lanl.gov>
2017-09-18 11:31:04 -0600

    deploy.sh: merge with no-edit

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ git checkout github
 git pull
 git checkout master
 git pull
-git merge --commit github
+git merge --no-edit github
 git diff --name-only HEAD^ | xargs touch
 make
 git commit -a -m "deploy html"
